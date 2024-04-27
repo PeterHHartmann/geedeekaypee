001ca50b49c6405fe081b35fd47c6e4748b3a4e9
Peter Helle Hartmann <Peter.Helle.Hartmann@gmail.com>
2024-04-27 14:02:00 +0200

    cleanup

--- app/ui/navigation/authlinks.tsx
+++ app/ui/navigation/authlinks.tsx
@@ -1,5 +1,4 @@
 import { auth, signOut } from '@/auth';
-import Link from 'next/link';
 import { ArrowRightStartOnRectangleIcon } from '@heroicons/react/24/outline';
 import NavLink from '@/app/ui/navigation/navlink';
 
@@ -12,7 +11,7 @@ export default async function AuthLinks() {
             <>
                 <li>
                     <NavLink
-                        name={user.name}
+                        name={user.name || 'User'}
                         href={`/user/${user.name}`}
                         icon='UserCircleIcon' />
                 </li>
@@ -37,39 +36,4 @@ export default async function AuthLinks() {
             <NavLink name='Login' href='/login' icon='ArrowRightEndOnRectangleIcon' />
         </li>
     );
-
-    // return (
-    //     <>
-    //         {user ? (
-    //             <>
-    //                 <li>
-    //                     <NavLink
-    //                         name={session.user.name}
-    //                         href={`/user/${session.user.name}`}
-    //                         icon='UserCircleIcon' />
-    //                 </li>
-    //                 <li>
-    //                     <form
-    //                         action={async () => {
-    //                             'use server';
-    //                             await signOut();
-    //                         }}
-    //                     >
-    //                         <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-sm text-slate-50 font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-    //                             <ArrowRightStartOnRectangleIcon className="w-6" />
-    //                             <p className="hidden md:block">Logout</p>
-    //                         </button>
-    //                     </form>
-    //                 </li>
-    //             </>
-
-    //         ) : (
-    //             <li>
-    //                 <NavLink name='Login' href='/login' icon='ArrowRightEndOnRectangleIcon' />
-    //             </li>
-    //         )
-    //         }
-    //     </>
-    // );
-
 }
