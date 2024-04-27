3f40fc088a19564dacb5dbd4f99386209cf000d4
Peter Helle Hartmann <Peter.Helle.Hartmann@gmail.com>
2024-04-27 13:44:20 +0200

    linting

--- app/ui/heroicon-dynamic.tsx
+++ app/ui/heroicon-dynamic.tsx
@@ -1,13 +1,13 @@
 import * as HIcons from '@heroicons/react/24/outline';
 
 export default function DynamicHeroIcon(props: { icon: string; }) {
-    // eslint-disable-next-line
     const { ...icons } = HIcons;
-    // eslint-disable-next-line
+    // @ts-ignore
     const TheIcon: JSX.Element = icons[props.icon];
 
     return (
         <>
+            {/* @ts-ignore */}
             <TheIcon className='w-6' aria-hidden="true" />
         </>
     );
