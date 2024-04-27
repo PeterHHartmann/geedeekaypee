8491cafec4c703e4325643a329488085844b022d
Peter Helle Hartmann <Peter.Helle.Hartmann@gmail.com>
2024-04-27 13:38:16 +0200

    temporary fix for linting issue

--- app/ui/heroicon-dynamic.tsx
+++ app/ui/heroicon-dynamic.tsx
@@ -1,6 +1,7 @@
 import * as HIcons from '@heroicons/react/24/outline';
 
 export default function DynamicHeroIcon(props: { icon: string; }) {
+    /* eslint-disable */
     const { ...icons } = HIcons;
     const TheIcon: JSX.Element = icons[props.icon];
 
