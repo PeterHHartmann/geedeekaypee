898cd4fc98e57425569918afe62bc3fc80d27f59
Peter Helle Hartmann <Peter.Helle.Hartmann@gmail.com>
2024-05-21 23:59:23 +0200

    custom color scheme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,53 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+            colors: {
+                primary: {
+                    50: "#fafafa",
+                    100: "#f4f4f5",
+                    150: "#efeff0",
+                    200: "#e4e4e7",
+                    250: "#dcdce0",
+                    300: "#d4d4d8",
+                    350: "#bbbbc1",
+                    400: "#a1a1aa",
+                    450: "#898992",
+                    500: "#71717a",
+                    550: "#62626b",
+                    600: "#52525b",
+                    650: "#494951",
+                    700: "#3f3f46",
+                    750: "#333338",
+                    800: "#27272a",
+                    850: "#202023",
+                    900: "#18181b",
+                    950: "#09090b"
+                },
+                deathknight: "#c41f3b",
+                druid: "#ff7c0a",
+                hunter: "#acd473",
+                mage: "#69ccf0",
+                paladin: "#f58cba",
+                priest: "#ffffff",
+                rogue: "#fff569",
+                shaman: "#0070de",
+                warlock: "#9482c9",
+                warrior: "#c79c6e"
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
