e10c1a041c1dbe9d648a8d8b3a3068bada17a69e
PeterHartmann <peter.helle.hartmann@gmail.com>
2024-05-30 19:42:14 +0200

    cleanup loading simulations

--- app/dashboard/@roster/@add_character/page.tsx
+++ app/dashboard/@roster/@add_character/page.tsx
@@ -1,11 +1,10 @@
 import { AddCharacterForm } from '@/components/roster/AddCharacterForm';
 import { fetchRolesForCharacterClasses, fetchCharacterClasses } from '@/lib/actions';
-import { sleep } from '@/lib/utils';
 
 export default async function AddCharacterSection() {
     const allCharacterClasses = await fetchCharacterClasses();
     const characterClassRoleOptions = await fetchRolesForCharacterClasses();
-    await sleep(5000);
+
     return (
         <AddCharacterForm characterClasses={allCharacterClasses} characterClassRolesOptions={characterClassRoleOptions} />
     );
--- app/dashboard/@roster/@list/page.tsx
+++ app/dashboard/@roster/@list/page.tsx
@@ -1,13 +1,11 @@
 import { fetchCharacterRoster } from '@/lib/actions';
 import { auth } from '@/auth';
 import { RosterCharacter } from '@/components/roster/RosterCharacter';
-import { sleep } from '@/lib/utils';
 
 export default async function RosterListPage() {
     const session = await auth();
     const user = session!.user!;
     const characterRoster = await fetchCharacterRoster(user.email);
-    await sleep(5000);
 
     return (
         <>
